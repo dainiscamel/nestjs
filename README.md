51710f6062d13b527eccda1570070a39e715033b
kangdain <juhyueon3@naver.com>
2021-08-22 21:56:53 +0900

    unit test & e2e test

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 ## Testing in Nest
 
  ### Testing ? 
- - .유닛테스팅 (=서비스에서 function과 같은 분리된 유닛을   테스트)  
+ - 유닛테스팅 (=서비스에서 function과 같은 분리된 유닛을   테스트)  
  - end-to-end(=모든 시스템을 테스트로 사용자가 취할만한 액션들을 처음 부터 끝까지 테스트)
  - package.json 파일을 보면 테스팅과 관련된 스크립트가 5가지 존재.
 ~~~
@@ -29,7 +29,7 @@
 1. "test": "jest",
    jest 는 자바스크립트를 쉽게 테스팅하는 npm 패키지.
    .spec.ts는 테스트를 포함한 파일 예를 들어 movies.controller.ts 를 테스트 하고 싶다면 movies.controller.spec.ts 파일이 존재해야한다.
-2. "test:watch": "jest --watch",
+2. "test:watch": "jest --watch"
     모든 테스트 파일들을 찾아내서 관찰
 3.  "test:cov": "jest --coverage"
     코드가 얼마나 테스팅 됐는지 알려준다.
--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,11 @@
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
-  // 테스트 하기 전에 실행되는 부분
+  // 테스트 하기전 실행 되는 부분.
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -14,5 +16,88 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', ()  => {
+      const result = service.getAll();
+      // 배열 인스턴스인지 확인.
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      }); 
+      const movie = service.getOne(1);
+      //toBeDefined () 사용 하여 변수가 정의되지 않았 는지 확인
+      expect(movie).toBeDefined();
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+    it('should throw a NotFoundException', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'Updated Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
